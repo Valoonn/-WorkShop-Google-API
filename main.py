2b7c1b54d302ec968ba4845db5608bd7b5d3acb8
Valoonn <59974644+Valoonn@users.noreply.github.com>
2021-06-19 16:08:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,5 @@ import sys, json
 
 exportPath = "./export/"
 
-jsonFile = GoogleMaps.getPlace("restaurant", "95 rue de boissy, 95320",  GoogleMaps.getCoords("95 rue de boissy, 95320"))
-GoogleSheet.createSheet(jsonFile, GoogleDrive.createFolder("workshop"))
-sheet = GoogleSheet.openSheet(jsonFile)
-jsonFile = open(exportPath + jsonFile + ".json", "r")
-jsonFile = json.loads(jsonFile.read())
-GoogleSheet.fillWithData(jsonFile["results"][0], sheet)
+
+## insert code here ##
